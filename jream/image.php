f4d125e62fbc124a298149b9d2c0473981a2562c
Jesse Boyer <aniyishay@gmail.com>
2012-10-04 08:56:57 -0400

    Image resize working

--- jream/image.php
+++ jream/image.php
@@ -15,9 +15,6 @@ class Image
     /** @var string $_originalFile */
     private $_originalFile = null;
 
-    /** @var string $_path */    
-    private $_path = null;
-    
     /** @var string $_newName */
     private $_newName = null;
     
@@ -45,6 +42,7 @@ class Image
         $info = pathinfo($this->_originalFile);
         $this->_ext = $info['extension'];
         $this->_origName = $info['filename'];
+        $this->_newName = $newName;
     }
     
     /**
@@ -59,7 +57,9 @@ class Image
             throw new \Exception('Dimensions must be an array of two');
         }
         
-        if ($this->_originalFile == null || $this->_path == null || $this->_newName == null)
+        if ($this->_originalFile == null 
+                || $this->_path == null 
+                || $this->_newName == null)
         {
             throw new \Exception('originalFile, path, newName must be set');
         }
@@ -89,7 +89,7 @@ class Image
                 $src = imagecreatefromjpeg ($this->_originalFile);
                 imagecopyresampled($image, $src, 0, 0, /*left*/0, /*top*/0, 
                         $width, $height, $origWidth, $origHeight);
-                $result = imagejpeg($image, $path . $this->_newName, /*compression*/90);
+                $result = imagejpeg($image, $this->_path . $this->_newName, /*compression*/90);
                 break;
             case "png":
                 imagealphablending($image, false);
@@ -99,7 +99,7 @@ class Image
                 imagecopyresampled($image, $src, 0, 0, /*left*/0, /*top*/0,
                         $width, $height, $origWidth, $origHeight);
                 imagealphablending($src, true);
-                $result = imagepng($image, $path . $this->_newName, /*compression*/9);
+                $result = imagepng($image, $this->_path . $this->_newName, /*compression*/9);
                 break;
             default:
                 return false;
